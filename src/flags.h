96663255d58f3c6ca75781528b9d24aafb0eccfd
Mini51 <youthfulslinky2@gmail.com>
2023-07-12 14:35:44 -0400

    added license as a command

--- src/flags.h
+++ src/flags.h
@@ -5,9 +5,11 @@
 #include <sstream>
 using namespace std;
 
-static char *read_file(const char *path) {
+static char *read_file(const char *path)
+{
 
-  if (!path) {
+  if (!path)
+  {
     cerr << "No path was provided \n";
     exit(74);
 
@@ -15,7 +17,8 @@ static char *read_file(const char *path) {
   }
 
   ifstream file(path, ios::binary);
-  if (!file) {
+  if (!file)
+  {
     cerr << "Could not open file \"" << path << "\"." << endl;
     exit(74);
   }
@@ -32,10 +35,12 @@ static char *read_file(const char *path) {
   return buffer;
 }
 
-inline void run_file(const char *path) {
+inline void run_file(const char *path)
+{
   const char *source = read_file(path);
   // Check to make sure that we have  a .zu file extension
-  if (strcmp(path + strlen(path) - 3, ".zu") != 0) {
+  if (strcmp(path + strlen(path) - 3, ".zu") != 0)
+  {
     cerr << "File \"" << path << "\" does not have a .zu extension." << endl;
     exit(65);
   }
@@ -48,29 +53,38 @@ inline void run_file(const char *path) {
     exit(70);
 }
 
-inline void flags(int argc, char *argv[]) {
-  if (argc == 2 && strcmp(argv[1], "--version") == 0) {
-    cout << "v0.0.1" << endl;
+inline void flags(int argc, char *argv[])
+{
+
+  if (argc == 2 && strcmp(argv[1], "--help") == 0)
+  {
+    cout << "Usage: " << argv[0] << " [options]" << endl;
+    cout << "Options:" << endl;
+    cout << "  --help\t\t\tPrints this help message" << endl;
+    cout << "  --repl\t\t\tStarts a REPL" << endl;
+    cout << "  --version\t\t\tPrints the version of the compiler" << endl;
+    cout << "  --license\t\t\tPrints the license of the Zura Lang" << endl;
     exit(0);
   }
-
-  if (argc == 2 && strcmp(argv[1], "--repl") == 0) {
+  if (argc == 2 && strcmp(argv[1], "--repl") == 0)
+  {
     char line[1024];
-    for (;;) {
+    for (;;)
+    {
       cout << "> ";
       if (!fgets(line, sizeof(line), stdin))
         break;
       interpret(line);
     }
   }
-
-  // Check if we have a --help flag
-  if (argc == 2 && strcmp(argv[1], "--help") == 0) {
-    cout << "Usage: " << argv[0] << " [options]" << endl;
-    cout << "Options:" << endl;
-    cout << "  --repl\t\t\tStarts a REPL" << endl;
-    cout << "  --version\t\t\tPrints the version of the compiler" << endl;
-    cout << "  --help\t\t\tPrints this help message" << endl;
+  if (argc == 2 && strcmp(argv[1], "--version") == 0)
+  {
+    cout << "v0.0.1" << endl;
+    exit(0);
+  }
+  if (argc == 2 && strcmp(argv[1], "--license") == 0)
+  {
+    cout << "Zura Lang is licenesed under GPL-3.0 license(https://www.gnu.org/licenses/gpl-3.0.en.html) " << endl;
     exit(0);
   }
 }
