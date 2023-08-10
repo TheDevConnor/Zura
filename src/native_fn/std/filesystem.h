08f88b83f93763c3ecbab6d4935f12801d6fda0d
connor <connorharris427@gmail.com>
2023-08-10 14:15:30 -0400

    fixed the one warning

--- src/native_fn/std/filesystem.h
+++ src/native_fn/std/filesystem.h
@@ -1,98 +1,110 @@
-#pragma once 
+#pragma once
 
-#include <iostream>
+#include <cmath>
+#include <cstddef>
 #include <cstring>
 #include <ctime>
-#include <cmath>
+#include <iostream>
 
 #include "../../compiler/object.h"
 #include "../../vm/vm.h"
 #include "../define_native.h"
 
 class Fs {
-    private:
-    static Value read_file_native(int arg_count, Value* args) {
-        if (arg_count != 1) return BOOL_VAL(false);
-        if (!IS_STRING(args[0])) return BOOL_VAL(false);
+private:
+  static Value read_file_native(int arg_count, Value *args) {
+    if (arg_count != 1)
+      return BOOL_VAL(false);
+    if (!IS_STRING(args[0]))
+      return BOOL_VAL(false);
 
-        ObjString* path = AS_STRING(args[0]);
-        FILE* file = fopen(path->chars, "rb");
-        if (file == NULL) {
-            return NIL_VAL;
-        }
-
-        fseek(file, 0L, SEEK_END);
-        size_t file_size = ftell(file);
-        rewind(file);
-
-        char* buffer = (char*)malloc(file_size + 1);
-        if (buffer == NULL) {
-            fclose(file);
-            return NIL_VAL;
-        }
-
-        size_t bytes_read = fread(buffer, sizeof(char), file_size, file);
-        if (bytes_read < file_size) {
-            free(buffer);
-            fclose(file);
-            return NIL_VAL;
-        }
-
-        buffer[bytes_read] = '\0';
-        fclose(file);
-        return OBJ_VAL(copy_string(buffer, (int)bytes_read));
+    ObjString *path = AS_STRING(args[0]);
+    FILE *file = fopen(path->chars, "rb");
+    if (file == NULL) {
+      return NIL_VAL;
     }
-    static Value write_file_native(int arg_count, Value* args) {
-        if (arg_count != 2) return BOOL_VAL(false);
-        if (!IS_STRING(args[0])) return BOOL_VAL(false);
-        if (!IS_STRING(args[1])) return BOOL_VAL(false);
 
-        ObjString* path = AS_STRING(args[0]);
-        ObjString* content = AS_STRING(args[1]);
+    fseek(file, 0L, SEEK_END);
+    size_t file_size = ftell(file);
+    rewind(file);
 
-        FILE* file = fopen(path->chars, "wb");
-        if (file == NULL) {
-            return NIL_VAL;
-        }
-
-        size_t bytes_written = fwrite(content->chars, sizeof(char), content->length, file);
-        if (bytes_written < content->length) {
-            fclose(file);
-            return NIL_VAL;
-        }
-
-        fclose(file);
-        return BOOL_VAL(true);
+    char *buffer = (char *)malloc(file_size + 1);
+    if (buffer == NULL) {
+      fclose(file);
+      return NIL_VAL;
     }
-    static Value generate_file_native(int arg_count, Value* args) {
-        if (arg_count != 1) return BOOL_VAL(false);
-        if (!IS_STRING(args[0])) return BOOL_VAL(false);
 
-        ObjString* path = AS_STRING(args[0]);
-        FILE* file = fopen(path->chars, "wb");
-        if (file == NULL) {
-            return NIL_VAL;
-        }
-
-        fclose(file);
-        return BOOL_VAL(true);
+    size_t bytes_read = fread(buffer, sizeof(char), file_size, file);
+    if (bytes_read < file_size) {
+      free(buffer);
+      fclose(file);
+      return NIL_VAL;
     }
-    static Value delete_file_native(int arg_count, Value* args) {
-        if (arg_count != 1) return BOOL_VAL(false);
-        if (!IS_STRING(args[0])) return BOOL_VAL(false);
 
-        ObjString* path = AS_STRING(args[0]);
-        if (remove(path->chars) != 0) {
-            return NIL_VAL;
-        }
+    buffer[bytes_read] = '\0';
+    fclose(file);
+    return OBJ_VAL(copy_string(buffer, (int)bytes_read));
+  }
+  static Value write_file_native(int arg_count, Value *args) {
+    if (arg_count != 2)
+      return BOOL_VAL(false);
+    if (!IS_STRING(args[0]))
+      return BOOL_VAL(false);
+    if (!IS_STRING(args[1]))
+      return BOOL_VAL(false);
 
-        return BOOL_VAL(true);
+    ObjString *path = AS_STRING(args[0]);
+    ObjString *content = AS_STRING(args[1]);
+
+    FILE *file = fopen(path->chars, "wb");
+    if (file == NULL) {
+      return NIL_VAL;
     }
-    public:
-    static void define_filesystem_natives() {
-        Natives::define_native("fsReadFile", read_file_native);
-        Natives::define_native("fsWriteFile", write_file_native);
-        Natives::define_native("fsGenerateFile", generate_file_native);
-        Natives::define_native("fsDeleteFile", delete_file_native);
+
+    size_t bytes_written =
+        fwrite(content->chars, sizeof(char), content->length, file);
+    if (bytes_written < (size_t)content->length) {
+      fclose(file);
+      return NIL_VAL;
     }
+
+    fclose(file);
+    return BOOL_VAL(true);
+  }
+  static Value generate_file_native(int arg_count, Value *args) {
+    if (arg_count != 1)
+      return BOOL_VAL(false);
+    if (!IS_STRING(args[0]))
+      return BOOL_VAL(false);
+
+    ObjString *path = AS_STRING(args[0]);
+    FILE *file = fopen(path->chars, "wb");
+    if (file == NULL) {
+      return NIL_VAL;
+    }
+
+    fclose(file);
+    return BOOL_VAL(true);
+  }
+  static Value delete_file_native(int arg_count, Value *args) {
+    if (arg_count != 1)
+      return BOOL_VAL(false);
+    if (!IS_STRING(args[0]))
+      return BOOL_VAL(false);
+
+    ObjString *path = AS_STRING(args[0]);
+    if (remove(path->chars) != 0) {
+      return NIL_VAL;
+    }
+
+    return BOOL_VAL(true);
+  }
+
+public:
+  static void define_filesystem_natives() {
+    Natives::define_native("fsReadFile", read_file_native);
+    Natives::define_native("fsWriteFile", write_file_native);
+    Natives::define_native("fsGenerateFile", generate_file_native);
+    Natives::define_native("fsDeleteFile", delete_file_native);
+  }
 };
