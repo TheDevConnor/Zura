17ef8e93815ea6577002b924f7b23691a6078b61
Kyle Anders <anderskyle89@gmail.com>
2023-08-11 21:04:40 -0700

    Removed pragma comment. Removed unneeded 'old' non-compiling macros. Rewrote a loop when freeing vm objects.

--- src/compiler/value.h
+++ src/compiler/value.h
@@ -62,41 +62,19 @@ struct Value {
   } as;
 };
 
-// Original
-#if 0
+#define IS_BOOL(value)    ((value).type == VAL_BOOL)
+#define IS_NIL(value)     ((value).type == VAL_NIL)
+#define IS_NUMBER(value)  ((value).type == VAL_NUMBER)
+#define IS_OBJ(value)     ((value).type == VAL_OBJ)
 
-#define IS_BOOL(value) ((value).type == VAL_BOOL)
-#define IS_NIL(value) ((value).type == VAL_NIL)
-#define IS_NUMBER(value) ((value).type == VAL_NUMBER)
-#define IS_OBJ(value) ((value).type == VAL_OBJ)
-
-#define AS_BOOL(value) ((value).as.boolean)
+#define AS_BOOL(value)   ((value).as.boolean)
 #define AS_NUMBER(value) ((value).as.number)
-#define AS_OBJ(value) ((value).as.obj)
-
-#define BOOL_VAL(value) ((Value){VAL_BOOL, {.boolean = value}})
-#define NIL_VAL ((Value){VAL_NIL, {.number = 0}})
-#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
-#define OBJ_VAL(object) ((Value){VAL_OBJ, {.obj = (Obj *)object}})
-
-#else
-
-// Edited
-#define IS_BOOL(value) ((value).type == VAL_BOOL)
-#define IS_NIL(value) ((value).type == VAL_NIL)
-#define IS_NUMBER(value) ((value).type == VAL_NUMBER)
-#define IS_OBJ(value) ((value).type == VAL_OBJ)
-
-#define AS_BOOL(value) ((value).as.boolean)
-#define AS_NUMBER(value) ((value).as.number)
-#define AS_OBJ(value) ((value).as.obj)
+#define AS_OBJ(value)    ((value).as.obj)
 
 #define BOOL_VAL(value)   Value{VAL_BOOL, {.boolean = value}}
 #define NIL_VAL           Value{VAL_NIL, {.number = 0}}
 #define NUMBER_VAL(value) Value{VAL_NUMBER, {.number = value}}
 #define OBJ_VAL(object)   Value{VAL_OBJ, {.obj = (Obj *)object}}
-
-#endif // Macro switch
 
 #endif
 
