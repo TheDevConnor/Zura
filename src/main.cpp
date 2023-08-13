0b2fa0df6fc6dbfc44825c0a2d6bafffea40cdd2
Connor <99085769+TheDevConnor@users.noreply.github.com>
2023-08-13 16:32:01 -0700

    Did a feew fixes on main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,3 @@
-/******************************************************************************
- * This file is part of Zura.
- *
- * Zura is free software: you may only use it under the terms of the
- * GNU General Public License as published by
- * the Free Software Foundation, of version 3 of the License, or
- * (at your option) any later version.
- *
- * Zura is distributed, WITHOUT ANY WARRANTY "AS IS"
- * without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Zura. If not, see <https://www.gnu.org/licenses/gpl-3.0.html>.
- *****************************************************************************/
-
 // Including necessary C standard libraries
 #include <stdio.h>  // Standard input/output functions
 #include <stdlib.h> // Standard library functions, e.g., memory allocation
