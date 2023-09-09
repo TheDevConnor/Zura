7c4574d6fac57031af758c338396deea03c0aaba
Connor <99085769+TheDevConnor@users.noreply.github.com>
2023-09-09 10:58:42 -0400

    seperated the vm into different files

---
+++ src/vm/vm_opcode_fn.hpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+#include "../debug/debug.hpp"
+#include "../common.hpp"
+#include "vm.hpp"
+
+static constexpr auto READ_BYTE = []() -> uint8_t { return *vm.ip++; };
+static constexpr auto READ_CONSTANT = []() -> Value { 
+    return vm.chunk->constants.values[READ_BYTE()]; 
+};
+
+static void BINARY_OP(double (*op)(double, double)) {
+    double b = pop();
+    double a = pop();
+    push(op(a, b));
+}
+
+using OpCodeHandler = void (*)();
+
+static void opConstant() {
+    Value constant = READ_CONSTANT();
+    push(constant);
+}
+
+static void opAdd() {
+    BINARY_OP([](double a, double b) { return a + b; });
+}
+
+static void opSubtract() {
+    BINARY_OP([](double a, double b) { return a - b; });
+}
+
+static void opMultiply() {
+    BINARY_OP([](double a, double b) { return a * b; });
+}
+
+static void opDivide() {
+    BINARY_OP([](double a, double b) { return a / b; });
+}
+
+static void opNegate() {
+    push(-pop());
+}
+
+static void opReturn() {
+    printValue(pop());
+    std::cout << std::endl;
+    exit(OK);
+}
+
+static OpCodeHandler opCodeHandlers[] = {
+    &opConstant,    // OP_CONSTANT
+    &opAdd,         // OP_ADD
+    &opSubtract,    // OP_SUBTRACT
+    &opMultiply,    // OP_MULTIPLY
+    &opDivide,      // OP_DIVIDE
+    &opNegate,      // OP_NEGATE
+    &opReturn,      // OP_RETURN
+};
--- src/vm/vm_run.hpp
+++ src/vm/vm_run.hpp
@@ -1,62 +1,9 @@
 #include <iostream>
 
 #include "../debug/debug.hpp"
+#include "vm_opcode_fn.hpp"
 #include "../common.hpp"
 #include "vm.hpp"
-
-static constexpr auto READ_BYTE = []() -> uint8_t { return *vm.ip++; };
-static constexpr auto READ_CONSTANT = []() -> Value { 
-    return vm.chunk->constants.values[READ_BYTE()]; 
-};
-
-static void BINARY_OP(double (*op)(double, double)) {
-    double b = pop();
-    double a = pop();
-    push(op(a, b));
-}
-
-using OpCodeHandler = void (*)();
-
-static void opConstant() {
-    Value constant = READ_CONSTANT();
-    push(constant);
-}
-
-static void opAdd() {
-    BINARY_OP([](double a, double b) { return a + b; });
-}
-
-static void opSubtract() {
-    BINARY_OP([](double a, double b) { return a - b; });
-}
-
-static void opMultiply() {
-    BINARY_OP([](double a, double b) { return a * b; });
-}
-
-static void opDivide() {
-    BINARY_OP([](double a, double b) { return a / b; });
-}
-
-static void opNegate() {
-    push(-pop());
-}
-
-static void opReturn() {
-    printValue(pop());
-    std::cout << std::endl;
-    exit(OK);
-}
-
-static OpCodeHandler opCodeHandlers[] = {
-    &opConstant,    // OP_CONSTANT
-    &opAdd,         // OP_ADD
-    &opSubtract,    // OP_SUBTRACT
-    &opMultiply,    // OP_MULTIPLY
-    &opDivide,      // OP_DIVIDE
-    &opNegate,      // OP_NEGATE
-    &opReturn,      // OP_RETURN
-};
 
 static Zura_Exit_Value run() {
     while (true) {
@@ -70,7 +17,7 @@ static Zura_Exit_Value run() {
         std::cout << std::endl;
         disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
     #endif
-        uint8_t instruction = READ_BYTE();
+        uint8_t instruction = *vm.ip++;
         if (instruction < sizeof(opCodeHandlers) / sizeof(opCodeHandlers[0])) {
             // execute the instruction
             opCodeHandlers[instruction]();
